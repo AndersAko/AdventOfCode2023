a0689453717b38d38dcad509cc2615ccd8b67858
Anders Kökeritz <anders.kokeritz@gmail.com>
2023-12-21 20:19:52 +0100

    beginning part 2

--- Day18/day18.py
+++ Day18/day18.py
@@ -67,49 +67,44 @@ def solve1(input: list[str]) -> int:
 
 
 def solve2(input: list[str]) -> int:
-    trench = dict()
+    trench = list()
     plan_re = re.compile(r"\w \d+ \(#(\w+)\)")
     row,col = 0,0
     for line in input:
         instruction = plan_re.findall(line)[0]
         length = int(instruction[:-1],16)
         dir = instruction[-1:]
-        for i in range(int(length)):
-            if dir == '0':
-                col += 1
-            elif dir == '1':
-                trench[(row,col)] = 'D'
-                row += 1
-            elif dir == '2':
-                col -= 1
-            elif dir == '3':
-                trench[(row,col)] = 'U'
-                row -= 1
-            else: 
-                print(f"Unknown dir {dir}") 
-            trench[(row,col)] = dir
+        if dir == '0':
+            col += length
+        elif dir == '1':
+            trench.append((col,row, row + length, 'D'))
+            row += length
+        elif dir == '2':
+            col -= length
+        elif dir == '3':
+            trench.append((col, row - length, row, 'U'))
+            row -= length
                 
-    min_r = min(r for r,_ in trench)
-    min_c = min(c for _,c in trench)
-    max_r = max(r for r,_ in trench)
-    max_c = max(c for _,c in trench)
+    min_r = min(r for _,r,_,_ in trench)
+    min_c = min(c for c,_,_,_ in trench)
+    max_r = max(r for _,r,_,_ in trench)
+    max_c = max(c for c,_,_,_ in trench)
     print (f"Trench limits: {(min_c, min_r)} -> {(max_c,max_r)}")
 
     inside = False
     last_dir = None
     count = 0
     for r in range(min_r, max_r+1):
-        for c in range(min_c, max_c+1):
-            if (r,c) in trench:
-                d = trench[(r,c)]
-                if d in ['U', 'D', '1', '3']:
-                    if d != last_dir:
-                        inside = not inside
-                    last_dir = d
-                count += 1
-            elif inside:
-                trench[(r,c)] = (None,'I')
-                count += 1
+        intersects = sorted([(c,d) for c,r1,r2,d in trench if r1<=r<=r2])
+        for col, d in intersects:
+            if d != last_dir: 
+                inside = not inside
+                if inside:
+                    last_col = col
+                else:
+                    count += col - last_col + 1
+            last_dir = d
+
     print(f"Total size of pit: {count}")
     # print_trench(trench)
 
